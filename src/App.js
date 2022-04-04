a251c5c9ea341b31c5186ed5029e631bafc230cf
hellolborg <hello@lanborg.ca>
2022-04-04 14:59:45 -0400

    Update routes

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter basename="/lanborg.github.io">
+    <BrowserRouter>
       <div className="App">
         <Header />
         <main>
