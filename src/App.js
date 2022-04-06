177b401764c1caa97e1bd9b9599ea4ad910cc85e
hellolborg <hello@lanborg.ca>
2022-04-05 22:56:47 -0400

    Fix BrowserRouter

--- src/App.js
+++ src/App.js
@@ -14,8 +14,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter> 
-    {/* basename="/lanborg.github.io" > */}
+    <BrowserRouter basename="/lanborg.github.io" >
       <div className="App">
         <Header />
         <main>
